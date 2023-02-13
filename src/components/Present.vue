3058b713ee32a7c77aa75b2525e0b778e0bcebf1
unknown <devselyan24@gmail.com>
2023-02-13 15:45:18 +0530

    images

--- src/components/Present.vue
+++ src/components/Present.vue
@@ -1,7 +1,30 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="h1">
-        <h1>Test</h1>
+    <div class="h1 text-center">
+        <h1>Present</h1>
+    </div>
+    <div class="info-container">
+        <div class="text text-center">
+            Teaching in Future School
+        </div>
     </div>
 </template>
+
+
+<style scoped>
+.info-container {
+  width: 1500px ;
+  height: 700px ;
+  background-color: purple;
+  margin: auto ;
+  margin-top: 2rem ;
+}
+
+.text {
+    font-size: large ;
+    font-weight: 600 ;
+    font-style: oblique;
+
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,10 @@ import { Present } from "@/components"
         <div class="col">
             <div class="content-section text-center">
                 <div class="title-section">Past</div>
+                <div class="present-text">
+                        Student in Future School
+                        <img src="https://cdn.pixabay.com/photo/2018/06/27/07/45/college-student-3500990__340.jpg" alt="">
+                    </div>
             </div>
         </div>
         <div class="col text-center">
@@ -16,7 +20,7 @@ import { Present } from "@/components"
                 <div class="present-info">
                     <div class="present-text">
                         Teacher in Future School
-                        <img src="" alt="">
+                    <img src="https://t3.ftcdn.net/jpg/02/65/18/30/360_F_265183061_NkulfPZgRxbNg3rvYSNGGwi0iD7qbmOp.jpg" alt="">
                     </div>
                     <div class="present-click">
                         <router-link to="/present" >Click to know more!</router-link>
@@ -27,6 +31,11 @@ import { Present } from "@/components"
         <div class="col text-center">
             <div class="content-section ">
                 <div class="title-section">Future</div>
+                <div class="present-text">
+                    College
+                    <img src="https://image.cnbcfm.com/api/v1/image/106922887-1628206615260-gettyimages-887132600-as1700__08.jpeg?v=1628206641" style="width: 531px; height: 360px;">
+                    <img src="" alt="">
+                </div>
             </div>
         </div>
     </div>  
@@ -53,4 +62,4 @@ import { Present } from "@/components"
     color:aquamarine ;
 }
 
-</style>
+</style >
