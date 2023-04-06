2a4d3bb137cdb2dfce92eda7a59768aafcf1c29e
unknown <devselyan24@gmail.com>
2023-04-06 18:00:01 +0530

    project card

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
+<script setup lang="ts">
+import { ProjectCard } from "@/components"
+
+const card_data = [
+    {
+        title: "Biology",
+        sub_title: "",
+    },
+    {
+        title: "Chemistry",
+        sub_title: "",
+    },
+    {
+        title: "Physics",
+        sub_title: "",
+    },
+    {
+        title: "Math",
+        sub_title: "",
+    },
+    {
+        title: "English",
+        sub_title: "",
+    },
+]
+</script>
+
 <template>
-    <div class="h1">
-        <router-link to="/" >Click to know more</router-link>
-    </div>
+    <div class="main-section filewrap">
+        <div class="col" v-for="i in 5" :key="i">
+            <div class="content-section text-center rounded shadow">
+                <MyCard :title="card_data[i-1].title" :sub_title="card_data[i-1].sub_title">
+                </MyCard>
+            </div>
+        </div>
+    </div> 
 </template>
